a217a8aff6742f67f017ed753e62557bdbaadfb3
Diego Jimenez <diego.jimenez@edu.tbz.ch>
2025-09-01 15:26:21 +0200

    Co-authored-by: ny-tim-c <ny-tim-c@users.noreply.github.com> Co-authored-by: ziemniaczex <ziemniaczex@users.noreply.github.com>

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ function mainloop(){
 
     
     if(scrollY < 227){
-        document.getElementById("androidico").style.scale = 1;
+        document.getElementById("androidico").style.scale();
         document.getElementById("androidico").style.left = scrollY*4;
         document.getElementById("androidico").style.top =  100 -scrollY;
         
     }
     else{
         document.getElementById("androidico").style.scale = 0.1;
-        document.getElementById("androidico").style.top =  auto;
+        document.getElementById("androidico").style.top =  10;
         document.getElementById("androidico").style.right = 0;
-        document.getElementById("androidico").setAttribute()
+        
                 
-            }
-        };
-
+    }
+    
+};
 
 setInterval(mainloop,1);

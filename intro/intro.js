8808338055afbc57629d64b45118c49d7d446146
Diego Jimenez <diego.jimenez@edu.tbz.ch>
2025-09-11 10:21:45 +0200

    Co-authored-by: ny-tim-c <ny-tim-c@users.noreply.github.com> Co-authored-by: ziemniaczex <ziemniaczex@users.noreply.github.com>

--- intro/intro.js
+++ intro/intro.js
@@ -1,19 +1,40 @@
 var y = 0;
+var o = Math.floor(Math. random()*10) + 1;
 function mainloop1(){
 
     if(y <= 9){
-        var x = 'gif/tile05.png';
-        x = 'gif/tile0' + y + '.png'
-        console.log(x);
+        var x = '/gif/tile05.png';
+        x = '/gif/tile0' + y + '.png'
+       
+         document.getElementById("androidgif").src = x;
+        y = y + 1;
     }else{
-        var x = 'gif/tile05.png';
-        x = 'gif/tile' + y + '.png'
-        console.log(x);
+        if(y < 19){
+            var x = '/gif/tile05.png';
+            x = '/gif/tile' + y + '.png'
+           
+             document.getElementById("androidgif").src = x;
+            y = y + 1;
+        }else{
+            
+            y = y + 1;
+        }
+    };
+    if(y == 21){
+        y = 0
     };
     
     
-    
-    document.getElementById("androidgif").src = x;
-    y = y + 1;
+   
 };
-setInterval(mainloop1,10);
+function redirect(){
+    if(o == 50){
+        window.location.href = '../main/main.html';
+        console.log(o)
+    }else{
+        o = o + 1;
+        console.log(o)
+    };
+};
+setInterval(mainloop1,100);
+setInterval(redirect,100)
